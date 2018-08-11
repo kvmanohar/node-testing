fe1430153f28f11a6760c209b6f962e5205b997e
Manohar Kurapati <kvmanohar@gmail.com>
2018-08-11 12:23:11 +0100

    replaced mocha with Jest testing framework

--- utility/utility.test.js
+++ utility/utility.test.js
@@ -1,15 +1,11 @@
 const utils = require("./utility");
 
-it('should add two numbers', () => {
+test('should add two numbers', () => {
     var res = utils.add(11, 33);
-    if (res !== 44) {
-        throw new Error(`Expected 44, but got ${res}`);
-    }
+    expect(res).toBe(44);
 });
 
-it('should square a number', () => {
+test('should square a number', () => {
     var res = utils.square(3);
-    if (res !== 9) {
-        throw new Error(`Expected 9 but got ${res}`);
-    }
+    expect(res).toBe(9);
 });
